6745478fd1065faeb4a09851491ac3edff5ac09c
micha42-dot <80447964+micha42-dot@users.noreply.github.com>
2026-04-18 16:32:25 +0200

    Update projects.js

--- projects.js
+++ projects.js
@@ -2,9 +2,7 @@ window.projectsData = [
   {
     id: "botanischergarten",
     title: "Botanischer Garten München",
-    description: "Eine fotografische Erkundung des Botanischen Gartens München-Nymphenburg<br><br>Der Botanische Garten München-Nymphenburg ist ein Ort, an dem wissenschaftliche Sammlung, gärtnerische Gestaltung und landschaftliche Atmosphäre zusammenkommen. Der 1914 eröffnete Garten gehört mit seinen großen Freiflächen und weitläufigen Gewächshäusern zu den wichtigsten botanischen Einrichtungen Europas.
-<br><br>
-Zwischen alten Gehölzen, Wasserflächen, Gewächshäusern und thematisch angelegten Gartenbereichen entfalten sich Pflanzen aus verschiedenen Klimazonen und Lebensräumen. Wege, Glasarchitektur und Vegetation erzeugen ein Wechselspiel aus Offenheit und Dichte, aus Struktur und Wachstum. Das wechselnde Licht prägt Blätter, Blüten und Räume immer wieder neu und schafft so einen Ort, an dem botanische Vielfalt und visuelle Wahrnehmung eng zusammenfinden.",
+    description: "Eine fotografische Erkundung des Botanischen Gartens München-Nymphenburg<br><br>Der Botanische Garten München-Nymphenburg ist ein Ort, an dem wissenschaftliche Sammlung, gärtnerische Gestaltung und landschaftliche Atmosphäre zusammenkommen. Der 1914 eröffnete Garten gehört mit seinen großen Freiflächen und weitläufigen Gewächshäusern zu den wichtigsten botanischen Einrichtungen Europas.<br><br>Zwischen alten Gehölzen, Wasserflächen, Gewächshäusern und thematisch angelegten Gartenbereichen entfalten sich Pflanzen aus verschiedenen Klimazonen und Lebensräumen. Wege, Glasarchitektur und Vegetation erzeugen ein Wechselspiel aus Offenheit und Dichte, aus Struktur und Wachstum. Das wechselnde Licht prägt Blätter, Blüten und Räume immer wieder neu und schafft so einen Ort, an dem botanische Vielfalt und visuelle Wahrnehmung eng zusammenfinden.",
     images: [
       "projekte/botanischergarten/01.jpg",
       "projekte/botanischergarten/02.jpg",
